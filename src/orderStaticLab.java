9e2966322ae4d10ea4079b8a01753f3c156fefaa
[Carter Hidalgo] <[carterhidalgo@letu.edu]>
2023-10-12 13:11:54 -0500

    oi

--- src/orderStaticLab.java
+++ src/orderStaticLab.java
@@ -1,3 +1,23 @@
+/*
+### COSC3523__OrderStatisticLab__Java
+
+Author(s): Carter Hidalgo  
+Course: COSC3523 Analysis of Algorithms  
+Project: COSC3523__OrderStatisticLab__Java  
+Date Due: 10/17/2023  
+
+Input: Generated random integers and a random ith order statistic  
+Output: Number of comparisons for RandomSelect and Median-Of-Five on the same input  
+Assumptions: N/A  
+Limitations: Performance is measured only in number of comparisons  
+Description: The program generates a random input and tests how quickly RandomSelect and Median-Of-Five can find the ith order statistic  
+
+Development Computer:  LG Gram 2022  
+Operating System: Windows 11 Home  
+Compiler: Java Programming Language Compiler
+Integrated Development Environment: Visual Studio Code  
+Operational Status: Operational  
+*/
 import java.util.Arrays;
 
 class orderStaticLab {
